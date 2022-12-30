885019c766c5add3b2278b50600d3962fa5455e9
daianaryan <88803959+daianaryan@users.noreply.github.com>
2022-12-29 21:21:06 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+/* import Exportacionjpg from './componentes/Exportacionjpg'; */
+import Imgmeme from './componentes/Imgmeme'
+import Textomeme from './componentes/Textomeme'
+import Api2 from './componentes/Api2'
+
+function App() {
+  return (
+    <div className="">        
+        <Api2 />                        
+    </div>
+  );
+}
+
+export default App;
