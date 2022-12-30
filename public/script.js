a07be3bde4c918b4f7b85ae4b4fdb17eba55d8ce
daianaryan <88803959+daianaryan@users.noreply.github.com>
2022-12-29 21:23:04 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,17 @@
+
+const seleccionar = (arg,arg2) =>{
+
+    let id = arg2;
+    let url = arg;
+
+    document.querySelector('.imagenDefault').src = arg;
+    document.querySelector('.imagenDefault').setAttribute('id',arg2);
+
+}
+
+
+
+
+/* const ul = document.querySelector('dropdown-menu'); */
+
+
